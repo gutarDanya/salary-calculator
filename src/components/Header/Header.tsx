b2814cae01e45682d6353c60b1c959129c1b67ae
Daniil_Ushakov <gutardanya@gmail.com>
2024-03-19 22:30:13 +0400

    add HEADER

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,8 @@ import styles from './Header.module.css';
 
 export const Header = () => {
     return (
-        <>
-        </>
+        <header className={styles.container}>
+            <img className={styles.logo} src='https://static.tildacdn.com/tild6665-3933-4965-b265-376530653061/logo.svg' alt='logo' />
+        </header>
     )
 }
